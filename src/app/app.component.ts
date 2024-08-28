cca6296229d5bce61a4a391ed950ecc75e1bb4e5
Alexandre Buttet-Allard <6227790@cstj.qc.ca>
2024-08-28 10:55:03 -0400

    origin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sphinx-ng';
+  essaiCourant:number = 0;
+  resultat:string = '';
+  nbEssais:number = 0;
+  nombre:number = 0;
+  init=false;
+
+  verifier()
+  {
+    if(!this.init)
+    {
+      this.nombre = 1 + Math.floor(Math.random() * 1000)
+      this.nbEssais = 0;
+      this.init = true;
+    }
+  }
 }
